bac2a836425213c2ee1e61bad0a92358a42fbfa8
Grace Tan <gracetan37@gmail.com>
2019-06-11 15:42:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude =>"hot-headed"}]
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -36,7 +36,8 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => [{:name =>"Steven",:age => "30", :attitude => "confused"}, {:name => "Nurse", :age => "44", :attitude =>  "worried"}]
+      :heroine_friends => [{:name =>"Steven",:age => "30", :attitude => "confused"}, {:name => "Nurse", :age => "44", :attitude =>  "worried"}
+      ]
    }
   }
 
