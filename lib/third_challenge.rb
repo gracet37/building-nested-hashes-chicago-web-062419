d5ea79aa0206f2f85b3e5e483e6c0e3d485d2a01
Grace Tan <gracetan37@gmail.com>
2019-06-11 15:40:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -36,7 +36,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => [{:name =>"Steven",:age => "30", :attitude => "confused"}, {:name => ":age => :attitude =>  }]
+      :heroine_friends => [{:name =>"Steven",:age => "30", :attitude => "confused"}, {:name => "Nurse", :age => "44", :attitude =>  "worried"}]
    }
   }
 
